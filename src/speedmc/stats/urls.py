e9ca3609ae3338e5f131c2f125e2bad4fe34ef5c
Tyler Hall <tylerwhall@gmail.com>
2010-11-13 18:35:39 -0500

    Stats: Add xmlrpc connection

--- src/speedmc/stats/urls.py
+++ src/speedmc/stats/urls.py
@@ -1,6 +1,9 @@
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import patterns, url
 import speedmc.settings
+from views import home, players
 
 urlpatterns = patterns('',
-    (r'^$', 'speedmc.stats.views.home'),
+    #(r'^$', 'speedmc.stats.views.home'),
+    url(r'^$', home),
+    url(r'^players$', players),
 )
--- src/speedmc/stats/views.py
+++ src/speedmc/stats/views.py
@@ -1,12 +1,48 @@
 # Create your views here.
-from django.http import HttpRequest, HttpResponse
-from django.template import Context, loader
-links = [
-('/stats/', 'Users Logged On'),
-('/stats/', 'Server Stats'),
-('/stats/', 'Minecraft Stats'),
-]
+from django.shortcuts import render_to_response
+from django.core.urlresolvers import reverse
+import xmlrpclib
+
+def getProxy():
+    return xmlrpclib.ServerProxy("http://user:password@evanwilliams:20012")
+
+def getPlayers():
+    p = getProxy()
+    try:
+        return p.player.getPlayers()
+    except IOError:
+        return None
+
+def getStats():
+    p = getProxy()
+    stats = [
+        ('MOTD', p.server.getMotd()),
+        ('Player Count', p.server.getPlayerCount()),
+        ('Player Limit', p.server.getPlayerLimit()),
+        ('Disallowed Items', p.server.getDisallowedItems()),
+        ('Hey0 Plugins', ','.join([pl['id'] for pl in p.server.getPlugins()])),
+    ]
+    return stats
+
+def links():
+    return [
+        (reverse(players), 'Users Logged On'),
+        (reverse(home), 'Server Stats'),
+        (reverse(home), 'Minecraft Stats'),
+    ]
+
 def home(request):
-    t = loader.get_template('stats/home.html')
-    c = Context({"links": links})
-    return HttpResponse(t.render(c))
+    d = {"links": links()}
+    try:
+        d['stats'] = getStats()
+    except IOError:
+        pass
+    return render_to_response('stats/home.html', d)
+
+def players(request):
+    d = {"links": links()}
+    try:
+        d['players'] = getPlayers()
+    except IOError:
+        pass
+    return render_to_response('stats/players.html', d)
