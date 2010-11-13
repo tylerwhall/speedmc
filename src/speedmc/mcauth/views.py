5e0dcbec6b50dad9482f12e019efac3ec1cc3182
Tyler Hall <tylerwhall@gmail.com>
2010-11-13 15:50:25 -0500

    Forgot to import exception class

--- src/speedmc/mcauth/views.py
+++ src/speedmc/mcauth/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import Context
 from django.views.decorators.csrf import csrf_exempt
+from django.utils.datastructures import MultiValueDictKeyError
 
 def checkserver(request):
     #c = Context({})
